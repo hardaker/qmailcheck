4a6042de452869b1ea7bfddea4da53f2ef31aa3d
Wes Hardaker <hardaker@users.sourceforge.net>
2010-05-19 21:33:50 -0700

    fixed display of data by properly return bogus qvarient in data()

--- incomingmailmodel.cpp
+++ incomingmailmodel.cpp
@@ -22,19 +22,27 @@ int IncomingMailModel::columnCount(const QModelIndex &parent) const
 
 Qt::ItemFlags IncomingMailModel::flags(const QModelIndex &index) const
 {
-    return 0;
+    return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 }
 
 QVariant IncomingMailModel::data(const QModelIndex &index, int role) const
 {
-    Q_UNUSED(index);
+    if (!index.isValid() || role != Qt::DisplayRole)
+        return QVariant();
     return QString::number(index.row() * index.column());
 }
 
 QVariant IncomingMailModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+#if 0
+    if (role == Qt::SizeHintRole)
+        return QSize(1,1);
+#endif
+    return QVariant();
+#if 0
     if (orientation == Qt::Horizontal)
         return QString("Column %1").arg("section");
     else
         return QString("Row %1").arg("section");
+#endif
 }
