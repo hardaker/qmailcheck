2197afd8504af3379e252c5a12d40d499dfce0f8
Wes Hardaker <hardaker@users.sourceforge.net>
2010-07-11 19:07:02 -0700

    filled out the mailSource

--- MailSource.cpp
+++ MailSource.cpp
@@ -4,3 +4,40 @@ MailSource::MailSource(QObject *parent) :
     QObject(parent)
 {
 }
+
+MailSource::MailSource(QObject *parent, const QString &mailServer, const QString &userName, const QString &passPhrase, bool ignoreCertErrors)
+    : QObject(parent), m_mailServer(mailServer), m_userName(userName), m_passPhrase(passPhrase), m_ignoreCertErrors(ignoreCertErrors)
+{
+}
+
+const QString &MailSource::mailServer() const {
+    return m_mailServer;
+}
+
+const QString &MailSource::userName() const {
+    return m_userName;
+}
+
+const QString &MailSource::passPhrase() const {
+    return m_passPhrase;
+}
+
+bool MailSource::ignoreCertErrors() const {
+    return m_ignoreCertErrors;
+}
+
+void MailSource::set_mailServer(const QString &mailServer) {
+    m_mailServer = mailServer;
+}
+
+void MailSource::set_userName(const QString &userName) {
+    m_userName = userName;
+}
+
+void MailSource::set_passPhrase(const QString &passPhrase) {
+    m_passPhrase = passPhrase;
+}
+
+void MailSource::set_ignoreCerterrors(bool ignoreErrors) {
+    m_ignoreCertErrors = ignoreErrors;
+}
--- MailSource.h
+++ MailSource.h
@@ -8,10 +8,27 @@ class MailSource : public QObject
     Q_OBJECT
 public:
     explicit MailSource(QObject *parent = 0);
+    MailSource(QObject *parent, const QString &mailServer, const QString &userName, const QString &passPhrase, bool ignoreCertErrors = false);
+
+    const QString &mailServer() const;
+    const QString &userName() const;
+    const QString &passPhrase() const;
+    bool          ignoreCertErrors() const;
+
+    void    set_mailServer(const QString &mailServer);
+    void    set_userName(const QString &userName);
+    void    set_passPhrase(const QString &passPhrase);
+    void    set_ignoreCerterrors(bool ignoreErrors);
 
 signals:
 
 public slots:
+
+private:
+    QString     m_mailServer;
+    QString     m_userName;
+    QString     m_passPhrase;
+    bool        m_ignoreCertErrors;
 
 };
 
