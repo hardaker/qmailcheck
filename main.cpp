afc694cd42ba72ca671d59615ff95656bb41d223
Wes Hardaker <opensource@hardakers.net>
2010-11-01 20:59:59 -0700

    remove debugging

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qDebug() << "before creation";
     QtIncoming w;
-    qDebug() << "after creation";
     w.show();
-    qDebug() << "should be shown";
     return a.exec();
 }
