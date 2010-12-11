efbc7a6e69147d735214ff081048bac4893f1639
Wes Hardaker <opensource@hardakers.net>
2010-12-10 20:58:45 -0800

    added a textpainter class

---
+++ TextPainter.h
@@ -0,0 +1,18 @@
+#ifndef TEXTPAINTER_H
+#define TEXTPAINTER_H
+
+#include <QStyledItemDelegate>
+
+class TextPainter : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit TextPainter(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // TEXTPAINTER_H
